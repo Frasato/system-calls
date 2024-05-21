7cd86a8d4db9dd78dff0d19c99d81b2ae3579360
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-05-21 16:45:14 -0300

    Create and config SingIn page

--- src/pages/SingIn/SingIn.js
+++ src/pages/SingIn/SingIn.js
@@ -1,7 +1,33 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 export default function SingIn(){
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    function handleEmail(e){
+        setEmail(e.target.value);
+    }
+
+    function handlePassword(e){
+        setPassword(e.target.value);
+    }
+
     return(
-        <>
-            <h1>Login Page</h1>
-        </>
+        <div className="contianer-center">
+            <div className="login">
+                <div className="logo-area">
+                    <img src="#" alt="logo"/>
+                </div>
+                <form>
+                    <h1>Login</h1>
+                    <input type="text" placeholder="Type your email" value={email} onChange={handleEmail}/>
+                    <input type="password" placeholder="***********" value={password} onChange={handlePassword}/>
+                    <input type="submit" value="Login"/>
+                    <Link to="/register">Create acount!</Link>
+                </form>
+            </div>
+        </div>
     );
 }

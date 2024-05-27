37ceb5fbfd41d7d4ebe7f0ba8b2712b75b4de39c
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-05-27 16:34:05 -0300

    Create Register Page

--- src/pages/SingUp/SingUp.js
+++ src/pages/SingUp/SingUp.js
@@ -1,7 +1,43 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 export default function SingUp(){
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [samePassword, setSamePassword] = useState('');
+
+    function handleEmail(e){
+        setEmail(e.target.value);
+    }
+
+    function handlePassword(e){
+        setPassword(e.target.value);
+    }
+
+    function handleSamePassword(e){
+        if(password === e.target.value){
+            setSamePassword(e.target.value);
+        }else{
+            alert('The password are not the same!');
+        }
+    }
+
     return(
-        <>
-            <h1>Register Page</h1>
-        </>
+        <div className="contianer-center">
+            <div className="login">
+                <div className="logo-area">
+                    <img src="#" alt="logo"/>
+                </div>
+                <form>
+                    <h1>Register</h1>
+                    <input type="text" placeholder="Type your email" value={email} onChange={handleEmail}/>
+                    <input type="password" placeholder="***********" value={password} onChange={handlePassword}/>
+                    <input type="password" placeholder="***********" value={samePassword} onChange={handleSamePassword}/>
+                    <input type="submit" value="Login"/>
+                    <Link to="/">Already have a acount?</Link>
+                </form>
+            </div>
+        </div>
     );
 }
